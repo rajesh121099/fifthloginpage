573c6e119e88cfc240bce9b1f04d416b6aba2b89
Sivakumar1103 <rajesh.r@aikyne.com>
2021-09-21 17:40:48 +0530

    21/09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,35 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { RegisterComponent } from './register/register.component';
+import { ForgetComponent } from './forget/forget.component';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    ForgetComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MatCardModule,MatButtonModule,
+    BrowserAnimationsModule,MatCardModule,MatButtonModule,MatCheckboxModule,MatIconModule,MatTabsModule,
+    RouterModule.forRoot([
+      {path:'',component:LoginComponent},
+      {path:'Login',component:LoginComponent},
+      {path:'Register',component:RegisterComponent},
+      {path:'Forget',component:ForgetComponent},
+      {path:'Account',component:AccountComponent},
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
